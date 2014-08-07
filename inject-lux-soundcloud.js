2f9d0e48a9fe46deb244b171447a8570e18949c6
Nick Sweeting <githubpirate@gmail.com>
2014-08-07 08:02:02 -0400

    Update inject-lux-soundcloud.js

--- inject-lux-soundcloud.js
+++ inject-lux-soundcloud.js
@@ -6,8 +6,6 @@ var luxinate = function(songurl) {
     });
 }
 
-document.luxinate = luxinate;
-
 var getluxbutton = function(buttongroup) {
     var songurl = "http://soundcloud.com" + buttongroup.parents('.sound').find('.soundTitle__title').attr('href')
     var button = $('<a href="#" onclick="luxinate(\'' + songurl + '\')" class="sc-button sc-button-small sc-button-responsive sc-button-download sc-button-lux" title="Download this track with Luxinate." tabindex="0">Lux</a>"');
@@ -26,21 +24,7 @@ var luxifypage = function() {
     });
 }
 
+document.luxinate = luxinate;
 $.support.cors = true;
 $(document).ajaxStop(luxifypage);
-
-// var last, diff;
-
-// var checkifxhrdone = function(xhrevent, request, settings) {
-//     luxlog(xhrevent, request, settings)
-//     if (last) {
-//         diff = event.timeStamp - last;
-//         luxlog("last XHR was " + diff + "seconds ago");
-//         if (diff > 500) {
-//             luxlog("Page is probably loaded.")
-//         }
-//     }
-//     last = event.timeStamp;
-// }
-
 
